65ebf6ccdec2a5b3662bbbb847bf67ea5092780c
Maurits Heetkamp <33858972+MauritsHeetkamp@users.noreply.github.com>
2019-05-27 00:56:51 +0200

    Initial Push
    All stuff

---
+++ Mode VR Project/Assets/Teleporter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class Teleporter : MonoBehaviour
+{
+    [SerializeField]SteamVR_Input_Sources teleportSource;
+    [SerializeField] SteamVR_Action_Boolean teleport;
+    public GameObject teleportHand;
+    public LayerMask teleportableLayers;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (teleport.GetState(teleportSource))
+        {
+            RaycastHit hitObject;
+            if(Physics.Raycast(teleportHand.transform.position, teleportHand.transform.forward, out hitObject, 1000, teleportableLayers, QueryTriggerInteraction.Ignore))
+            {
+
+            }
+        }
+    }
+    public void Teleport(Vector3 position)
+    {
+
+    }
+}
