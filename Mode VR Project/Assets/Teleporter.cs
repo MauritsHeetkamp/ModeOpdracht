ab8986cc097021b6e364f7767a8630ff73252b93
Maurits Heetkamp <33858972+MauritsHeetkamp@users.noreply.github.com>
2019-05-29 01:23:29 +0200

    Update
    Yes all functions in here

--- Mode VR Project/Assets/Teleporter.cs
+++ Mode VR Project/Assets/Teleporter.cs
@@ -9,6 +9,12 @@ public class Teleporter : MonoBehaviour
     [SerializeField] SteamVR_Action_Boolean teleport;
     public GameObject teleportHand;
     public LayerMask teleportableLayers;
+    public string teleportableTag;
+
+
+    public GameObject teleportObject;
+    public Transform mainCamera;
+    GameObject teleportingObject;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,15 +26,32 @@ public class Teleporter : MonoBehaviour
     {
         if (teleport.GetState(teleportSource))
         {
+            if (teleport.GetStateDown(teleportSource))
+            {
+                teleportingObject = Instantiate(teleportObject);
+            }
             RaycastHit hitObject;
             if(Physics.Raycast(teleportHand.transform.position, teleportHand.transform.forward, out hitObject, 1000, teleportableLayers, QueryTriggerInteraction.Ignore))
             {
-
+                if(hitObject.transform.tag == teleportableTag)
+                {
+                    teleportingObject.transform.position = hitObject.point;
+                }
+            }
+        }
+        else
+        {
+            if (teleport.GetStateUp(teleportSource))
+            {
+                Teleport(teleportingObject.transform.position);
+                Destroy(teleportingObject);
             }
         }
     }
     public void Teleport(Vector3 position)
     {
-
+        position.x -= mainCamera.position.x;
+        position.z -= mainCamera.position.z;
+        transform.position = position;
     }
 }
